06c73fe2ff742988c47a71063982dcd553cecb78
rotonda <rotondwamulelu@gmail.com>
2021-06-06 11:01:07 +0200

    Init

---
+++ src/components/home/sectionHeader/styles/sectionHeader.js
@@ -0,0 +1,60 @@
+import styled from "styled-components";
+import bg from "../../../../assets/portia_2.jpeg";
+import { textColor, textColor1 } from "../../../../colors/colors";
+
+export const Section = styled.section``;
+
+export const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  background-image: url(${bg});
+  background-size: contain;
+  height: 59rem;
+  color: #ffff;
+`;
+
+export const Heading4 = styled.h4`
+  padding: 2rem;
+  font-size: 2.1rem;
+`;
+
+export const Heading1 = styled.h1`
+  padding-bottom: 1rem;
+  font-size: 2.6rem;
+`;
+
+export const Download = styled.a`
+  width: 15rem;
+  height: 5.4rem;
+  background-color: transparent;
+  text-align: center;
+  border-radius: 5rem;
+  text-decoration: none;
+  border: 3px solid #5385c2;
+  padding-top: 1.4rem;
+  color: #5385c2;
+  font-size: 1.6rem;
+  -webkit-transition: all 0.5s;
+  transition: all 0.5s;
+  -webkit-transition: color 0.15s ease-in-out,
+    background-color 0.15s ease-in-out, border-color 0.15s ease-in-out,
+    box-shadow 0.15s ease-in-out;
+  transition: color 0.15s ease-in-out, background-color 0.15s ease-in-out,
+    border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  &: hover {
+    background-color: #5385c2;
+    color: #ffff;
+  }
+`;
+
+export const Image = styled.img``;
+
+export const Text = styled.p`
+  font-size: 1.4rem;
+  width: 35rem;
+  text-align: center;
+  margin-bottom: 1.4rem;
+  line-height: 1.5;
+`;
---
+++ src/data/mmData.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+export const MMData = [
+  {
+    day: "Important and urgent",
+    task: [
+      "Complete my diploma in electrical engineering (computer systems)",
+      "Practise electrical engineering and physics for a better understanding",
+      "Study for electrical engineering and physics ",
+      "Practice maths test on 7 June",
+      "Prepare for engineering skills assignment",
+      "Stay up to date with my academics",
+    ],
+  },
+  {
+    day: "Important and not urgent",
+    task: [
+      "Making new friends",
+      "Look for a job",
+      "Being successful at the end of the day",
+      "Have valid business ideas",
+    ],
+  },
+  {
+    day: "Unimportant and urgent",
+    task: [
+      "Getting a car",
+      "Gambling",
+      "Taking part in school events",
+      "Participating in sports",
+    ],
+  },
+  {
+    day: "Unimportant and not urgent",
+    task: ["Watch movies and series", "Parties", "Dating", "Trying to fit in"],
+  },
+];
